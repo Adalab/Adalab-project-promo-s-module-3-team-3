0cbe2de309ae0a563680628346fc966b7609f92e
laurapascual <laurapascualmateo@gmail.com>
2023-03-16 15:05:37 +0100

    add landing scss

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
 import objectToExport from '../services/LocalStorage';
+import '../styles/layouts/LandingPage.scss'
+
 const LandingPage = () => {
   return (
-    <main>
-      <h1>Proyectos y ya estaría</h1>
-      <h2>Escaparate en línea para recoger ideas a través de la tecnología</h2>
-      <Link to="/CreateProject"><button>NUEVO PROYECTO</button> </Link>
+    <main className='landingPage'>
+      <h1 className='landingPage__title'>Proyectos y ya estaría</h1>
+      <h2 className='landingPage__subtitle'>Escaparate en línea para recoger ideas a través de la tecnología</h2>
+      <Link to="/CreateProject"><button className='landingPage__newBtn'>NUEVO PROYECTO</button> </Link>
       <button>VER PROYECTOS</button>  
     </main>
   );
