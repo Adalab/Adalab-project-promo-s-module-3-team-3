f79c53ec61e7185ce74a842261b407f80fbe432f
laurapascual <laurapascualmateo@gmail.com>
2023-03-17 14:40:34 +0100

    Add landing scss

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,11 +7,11 @@ import Card from "./Preview/Card";
 const LandingPage = ({cards}) => {   
   return (
     <main className='landingPage'>
-      <h1 className='landingPage__title'>Proyectos y ya estaría</h1>
+      <h1 className='landingPage__title'>Proyectos y ya estaría </h1>
       <h2 className='landingPage__subtitle'>Escaparate en línea para recoger ideas a través de la tecnología</h2>
       <Link to="/CreateProject"><button className='landingPage__newBtn'>NUEVO PROYECTO</button> </Link>
       <button>VER PROYECTOS</button>  
-      <div className="preview">{cards.map((card, index) => <Card data={card} key={index}/>)}</div>
+      <div className="landingDiv">{cards.map((card, index) => <Card data={card} key={index}/>)}</div>
       {cards.length === 0 && <p>No hay tarjetas que mostrar</p>}
     </main>
   );
