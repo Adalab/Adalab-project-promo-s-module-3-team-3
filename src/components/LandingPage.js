7ec453fc0f1e10feea72af868b168366a81aad4e
ttkapostol <ttkapostolaki@gmail.com>
2023-03-18 20:54:40 +0100

    Add basic button scss to Landing

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,18 +1,24 @@
-import { Link } from 'react-router-dom';
-import '../styles/layouts/LandingPage.scss'
+import { Link } from "react-router-dom";
+import "../styles/layouts/LandingPage.scss";
 import Card from "./Preview/Card";
 
-
-
-const LandingPage = ({cards}) => {   
+const LandingPage = ({ cards }) => {
   return (
-    <main className='landingPage'>
-      <h1 className='landingPage__title'>Proyectos y ya estaría </h1>
-      <h2 className='landingPage__subtitle'>Escaparate en línea para recoger ideas a través de la tecnología</h2>
-      <Link to="/CreateProject"><button className='landingPage__newBtn'>NUEVO PROYECTO</button> </Link>
-      <button>VER PROYECTOS</button>  
-      <div className="landingDiv">{cards.map((card, index) => <Card data={card} key={index}/>)}</div>
-      {cards.length === 0 && <p>No hay tarjetas que mostrar</p>}
+    <main className="landingPage">
+      <h1 className="landingPage__title">Proyectos y... ¡ya estaría! </h1>
+      <h2 className="landingPage__subtitle">
+        Escaparate en línea para recoger ideas a través de la tecnología
+      </h2>
+      <Link to="/CreateProject">
+        <button className="btn">NUEVO PROYECTO</button>{" "}
+      </Link>
+      <button className="btn">VER PROYECTOS</button>
+      <div className="landingDiv">
+        {cards.map((card, index) => (
+          <Card data={card} key={index} />
+        ))}
+      </div>
+      {cards.length === 0 && <p className="msg">No hay tarjetas que mostrar</p>}
     </main>
   );
 };
